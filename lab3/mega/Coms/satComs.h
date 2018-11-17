12f4752fe774e7dc79efcec041f438e1984c0a38
Bogdan Tudos <bogdantudos@gmail.com>
2018-11-16 17:07:32 -0800

    Added structs and some comments

---
+++ lab3/mega/Coms/satComs.h
@@ -0,0 +1,89 @@
+#ifndef SATCOMS_H
+#define SATCOMS_H
+
+struct TCB {
+  void* taskData;
+  void (*myTask)(void*);
+  struct TCB* next;
+  struct TCB* prev;
+};
+typedef struct TCB TCB;
+
+struct powerSubsystemData {
+  int* solarPanelState;
+  int* deploy;
+  int* retract;
+  unsigned int* batLevel;
+  unsigned short* pwrCon;
+  unsigned short* pwrGen;
+};
+typedef struct powerSubsystemData powerSubsystemData;
+
+struct thrusterSubsystemData {
+  unsigned int* thrusterCommand;
+  unsigned short* fuelLevel;
+};
+typedef struct thrusterSubsystemData thrusterSubsystemData;
+
+struct satelliteComsData {
+  int* fuelLow;
+  int* batLow;
+  int* solarPanelState;
+  unsigned int* batLevel;
+  unsigned short* fuelLevel;
+  unsigned short* pwrCon;
+  unsigned short* pwrGen;
+  unsigned int* thrusterCommand;
+};
+typedef struct satelliteComsData satelliteComsData;
+
+struct consoleDisplayData {
+  int* fuelLow;
+  int* batLow;
+  int* solarPanelState;
+  unsigned int* batLevel;
+  unsigned short* fuelLevel;
+  unsigned short* pwrCon;
+  unsigned short* pwrGen;
+ unsigned short* transportDist;
+};
+typedef struct consoleDisplayData consoleDisplayData;
+
+struct warningAlarmData {
+  int* fuelLow;
+  int* batLow;
+  int* solarPanelState;
+  unsigned int* batLevel;
+  unsigned short* fuelLevel;
+  int* batteryOverheating;
+};
+typedef struct warningAlarmData warningAlarmData;
+
+struct vehicleCommsData {
+  char* command;
+  char* response;
+};
+typedef struct vehicleCommsData vehicleCommsData;
+
+struct consoleKeypadData {
+  int* motorSpeedInc;
+  int* motorSpeedDec;
+};
+typedef struct consoleKeypadData consoleKeypadData;
+
+struct solarPanelControlData {
+  int* solarPanelState;
+  int* deploy;
+  int* retract;
+  int* motorSpeedInc;
+  int* motorSpeedDec;
+  unsigned short* motorDrive;
+};
+typedef struct solarPanelControlData solarPanelControlData;
+
+struct batteryTempData {
+  int* battTemp;
+};
+typedef struct batteryTempData batterTempData;
+
+#endif
